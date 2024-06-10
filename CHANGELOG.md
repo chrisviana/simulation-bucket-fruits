c77debc373fd12f0b69ea3b850ab59cbdb65a894
Christian Viana <viana2015christian@gmail.com>
2024-06-10 14:28:47 -0300

    Finalizado pagina de cadastro de balde

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### Added
+
+- [**issue-1**](https://github.com/chrisviana/simulation-bucket-fruits/issues/1) - Criar interface para registro de baldes
--- src/components/balde/index.tsx
+++ src/components/balde/index.tsx
@@ -1,14 +1,37 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
 import './style.scss'
 
+type FormularioData = {
+	capacidade: number
+}
+
+
 export const Balde = () => {
+
+	const { register, handleSubmit, formState: { errors }, reset } = useForm<FormularioData>()
+	
+	const salvarBalde: SubmitHandler<FormularioData> = data => {
+		console.log(data)
+
+		reset()
+	}
+
+
 	return (
 		<div className="container-balde">
 			<h1>Criar Balde</h1>
-			<div>
+			<form onSubmit={handleSubmit(salvarBalde)}>
 				<span>Capacidade Máxima</span>
-				<input type='number' />
+				<input type='number'
+					{...register('capacidade', { 
+						required: 'A capacidade é obrigatória', 
+						valueAsNumber: true,
+						validate: value => value > 0 || 'A capacidade deve ser maior que zero.'
+					})}
+				/>
+				{errors.capacidade && <p>{errors.capacidade.message}</p>}
 				<button type='submit'>Salvar</button>
-			</div>
+			</form>
       
 		</div>
 	)
