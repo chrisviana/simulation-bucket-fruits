a63c30727016abfa6b0aae7b55c55b6f56e2303c
Christian Viana <viana2015christian@gmail.com>
2024-06-10 20:26:04 -0300

    Criado interface para registrar as frutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Balde } from './components/balde'
 import './app.scss'
+import { Frutas } from './components/frutas'
 export const App = () => {
 
 	return (
 		<div className='container-app'>
 			<Balde />
+			<Frutas />
+			<div>
+				<h1>Lista de Balde</h1>
+			</div>
 		</div>
 	)
 }
